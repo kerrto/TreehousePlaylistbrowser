c4445b2b6a09dfcab445357f5ef2084e7a141e74
Kerry Toonen <kerrytoonen@neva-bassingthwaites-macbook-pro.local>
2015-11-08 19:01:23 +0900

    Initial Commit

---
+++ Algorhythm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Algorhythm
+//
+//  Created by Kerry Toonen on 2015-11-08.
+//  Copyright © 2015 Kerrto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Algorhythm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Algorhythm
+//
+//  Created by Kerry Toonen on 2015-11-08.
+//  Copyright © 2015 Kerrto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
